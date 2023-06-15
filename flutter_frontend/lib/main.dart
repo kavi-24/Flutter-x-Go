599496c0251234ae8dd4162bcaaadfd6efc3342b
kavi-24 <--replace-all>
2023-06-16 03:23:55 +0530

    first commit

---
+++ flutter_frontend/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/product_service.dart';
+import 'package:flutter_frontend/product_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final _productService = ProductService();
+
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Product List';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xffdddddd)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: FutureBuilder<List<Product>>(
+          future: _productService.getProducts(),
+          builder: (context, snapshot) {
+            var products = snapshot.data ?? [];
+
+            if(!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            return ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                var product = products[index];
+                return ListTile(
+                  title: Text(products[index].name),
+                  subtitle: Text('#${product.id} ${product.description}'),
+                  trailing: Text('\$${product.price}')
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
